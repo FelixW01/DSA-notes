b420eb1f60f61df2ae6e76bb0f6092a89a0a6331
Felix W <felixwillem01@yahoo.com>
2024-03-14 19:09:26 -0400

    worked on further understanding big O notation

--- BigONotation.js
+++ BigONotation.js
@@ -35,7 +35,8 @@ var t2 = performance.now();
 // Solution 2 is clearly faster and more efficient than Solution 1
 addUpTo2(1000000000, 'Solution 2');
 
-console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds.`)
+// console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds.`)
+
 // Counting the time it takes doesn't accurately represent the performance of the code
 // Big O counts the number of simple operations the computer has to perform instead
 // Big O notation is a way to formalize fuzzy counting
@@ -65,4 +66,42 @@ function printAllPairs(n) {
             console.log(i, j);
         }
     }
+}
+
+// Simplifying Big O Expressions
+// Constants don't matter in the broader sense of Big O
+// O(2n) = O(n) 
+// O(500) = O(1) 
+// O(13n2) = O(n2) 
+// O(n2 + 5n + 8) = O(n2)
+
+// Big O Shorthands
+// Arithemethic operations are constant
+// Variable assignment is constant
+// Accessing elements in an array (by index) or object (by key) is constant
+// In a loop, the complexity is the length of the loop times the complexity of whatever happens inside of the loop
+
+// Space Complexity in JS
+// Most primitives (booleans, numbers, undefined, null) are constant space
+// Strings require O(n) space (where n is the string length)
+// Reference types are generally O(n), where n is the length (for arrays) or the number of keys (for objects)
+
+// o(1) - Constant Space
+// The function creates a single variable which is independent of the size of the input
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+// o(n) - Linear Space
+// The function creates a new array which is dependent on the size of the input
+function double(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(2 * arr[i]);
+    }
+    return newArr;
 }
