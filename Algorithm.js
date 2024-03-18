09d4d50ef7634f90ea4190588adb37133eb3e4be
Felix W <felixwillem01@yahoo.com>
2024-03-17 22:02:10 -0400

    worked on algorithms and steps to properly solve a problem

--- Algorithm.js
+++ Algorithm.js
@@ -16,7 +16,7 @@
 // (You may not be able to answer this question until you set about solving the problem. That's okay; it's still worth considering the question at this early stage.)
 // 5. How should I label the important pieces of data that are a part of the problem?
 
-// Write a function which takes two numbers and returns their sum.
+// Write a function which takes two numbers and returns their sum. //
 
 // Can I restate the problems in my own words?
 // Implement addition
@@ -32,3 +32,41 @@
 function addNumbers(num1, num2) {
     return num1 + num2;
 }
+
+// Break it down
+// Excplicitly write out the steps you need to take.
+// Pseudo code
+
+function addNumbers(str) {
+    // do something
+    // return an object with keys that are lowercase alphanumeric characters in the string; values should be the number of times the character appears in the string
+}
+function charCount(str) {
+    // make object to return at end
+    let result = {};
+    // loop over string, for each character...
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i].toLowerCase();
+        // if the char is a number/letter AND is a key in object, add one to count
+        if (result[char] > 0) {
+            result[char]++;
+        } 
+        // if char is a number/letter AND not in object, add it and set value to 1
+        else {
+            result[char] = 1;
+        };
+    }
+    // return object at end
+        return result;
+        // if char is something else (space, period, etc.) don't do anything
+}
+console.log(charCount("Hi there!"));
+
+// Refactoring
+// Can you check the result?
+// Can you derive the result differently?
+// Can you understand it at a glance?
+// Can you use the result or method for some other problem?
+// Can you improve the performance of your solution?
+// Can you think of other ways to refactor?
+// How have other people solved this problem?
